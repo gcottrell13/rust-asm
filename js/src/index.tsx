fb74c22d80f2e8ee1025106d530656da42afb1d4
gage <gc362@nau.edu>
2018-05-28 21:06:55 -0700

    didn't stage this I guess

--- js/src/index.tsx
+++ js/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { DebuggerApplication } from "./components/debugger";
-import { loadWasmAsync, GetWasmExports } from "./utils/webAssembly";
+import { loadWasmAsync } from "./utils/webAssembly";
 import { getWasmImports } from "./utils/wasmImports";
 
 loadWasmAsync("./dsl_wasm.wasm", getWasmImports()).then(() => {
