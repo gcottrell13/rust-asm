c7afc049b2381317752f5c6f75263af81a368588
Gage <inquisitorpinkie@gmail.com>
2019-01-01 13:58:43 -0700

    formatting

--- js/src/utils/generalUtils.ts
+++ js/src/utils/generalUtils.ts
@@ -1,48 +1,48 @@
 
 export function TextFromBlobAsync(blob: Blob): Promise<string> {
-    return new Promise((resolve, reject) => {
-        var reader = new FileReader();
-        reader.onload = function() {
-            resolve(reader.result as string);
-        }
-        reader.readAsText(blob);
-    });
+	return new Promise((resolve, reject) => {
+		var reader = new FileReader();
+		reader.onload = function () {
+			resolve(reader.result as string);
+		};
+		reader.readAsText(blob);
+	});
 }
 
 export function StringToASCII(text: string): Uint8Array {
-    return (new TextEncoder()).encode(text);
+	return (new TextEncoder()).encode(text);
 }
 
 export function contains<T>(arr: ArrayLike<T>, value: T): boolean {
-    for(let i = 0; i < arr.length; i++) {
-        if (arr[i] === value) {
-            return true;
-        }
-    }
-    return false;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === value) {
+			return true;
+		}
+	}
+	return false;
 }
 
 export function toInt(n: string): number {
-    const i = parseInt(n);
-    if (isNaN(i)) {
-        throw new Error(`Cannot parse '${n}' to an integer`);
-    }
-    return i;
+	const i = parseInt(n, 10);
+	if (isNaN(i)) {
+		throw new Error(`Cannot parse '${n}' to an integer`);
+	}
+	return i;
 }
 
 export function group<T>(n: number, arr: ArrayLike<T>): T[][] {
-    let groups: T[][] = [];
-    let lastGroup: T[] = [];
-    for(let i = 0; i < arr.length; i++) {
-        if (lastGroup.length === 0) {
-            groups.push(lastGroup);
-        }
+	let groups: T[][] = [];
+	let lastGroup: T[] = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (lastGroup.length === 0) {
+			groups.push(lastGroup);
+		}
 
-        lastGroup.push(arr[i]);
+		lastGroup.push(arr[i]);
 
-        if (lastGroup.length >= n) {
-            lastGroup = [];
-        }
-    }
-    return groups;
+		if (lastGroup.length >= n) {
+			lastGroup = [];
+		}
+	}
+	return groups;
 }
--- js/src/utils/utilTypes.ts
+++ js/src/utils/utilTypes.ts
@@ -1,22 +1,22 @@
 export interface SMap<T> {
-    [p: string]: T
+	[p: string]: T;
 }
 
-export type Dict<K extends string | number | symbol, V> = {[p in K]: V;};
+export type Dict<K extends string | number | symbol, V> = { [p in K]: V; };
 
 export type keyable = string | number | symbol;
 
 interface MapFunction<TIn, TOut> {
-    (v: TIn): TOut;
+	(v: TIn): TOut;
 }
 
 interface FilterFunction<TIn> {
-    (v: TIn): boolean;
+	(v: TIn): boolean;
 }
 
 interface Matching<T> {
-    Just?: (value: T) => void, 
-    None?: () => void,
+	Just?: (value: T) => void;
+	None?: () => void;
 }
 
 type Optional<T> = T | null | undefined;
@@ -25,158 +25,158 @@ export interface Maybe<T> {
     /**
      * Gives either the value if Just<T> or null if None.
      */
-    value(): T | null;
+	value(): T | null;
 
     /**
      * UNSAFE Gives the value. Use only if you know for sure that the value will exist.
      */
-    unwrap(): T;
+	unwrap(): T;
 
     /**
      * Returns whether or not there is a value.
      */
-    hasValue(): boolean;
+	hasValue(): boolean;
 
     /**
      * Applies the given function to the value, if it exists.
      * @param fn the mapping function
      */
-    map<R>(fn: MapFunction<T, Optional<R>>): Maybe<R>;
+	map<R>(fn: MapFunction<T, Optional<R>>): Maybe<R>;
 
     /**
      * Provides a default value if none
      * @param fn 
      */
-    else(fn: () => T): Maybe<T>;
+	else(fn: () => T): Maybe<T>;
 
     /**
      * Gets the given property, if it exists.
      * @param propName the property to get
      */
-    prop<O extends keyof T>(propName: O): Maybe<T[O]>;
-    
+	prop<O extends keyof T>(propName: O): Maybe<T[O]>;
+
     /**
      * Calls the appropriate function given the state of the Maybe
      * @param m 
      */
-    match(m: Matching<T>): Maybe<T>;
+	match(m: Matching<T>): Maybe<T>;
 
     /**
      * Returns a value if it matches.
      * @param fn fn
      */
-    filter(fn: FilterFunction<T>): Maybe<T>;
+	filter(fn: FilterFunction<T>): Maybe<T>;
 }
 
 export class None<T> implements Maybe<T> {
-    value() {
-        return null;
-    }
+	value() {
+		return null;
+	}
 
-    unwrap() {
-        return null as any as T;
-    }
+	unwrap() {
+		return null as any as T;
+	}
 
-    hasValue() {
-        return false;
-    }
+	hasValue() {
+		return false;
+	}
 
-    map<R>(mapFn: MapFunction<T, Optional<R>>) {
-        return new None<R>();
-    }
+	map<R>(mapFn: MapFunction<T, Optional<R>>) {
+		return new None<R>();
+	}
 
-    match(m: Matching<T>): Maybe<T> {
-        if (m.None) {
-            m.None();
-        }
-        return this;
-    }
-    
-    else(fn: () => T): Maybe<T> {
-        return Maybe(fn());
-    }
+	match(m: Matching<T>): Maybe<T> {
+		if (m.None) {
+			m.None();
+		}
+		return this;
+	}
 
-    prop<O extends keyof T>(propName: O): Maybe<T[O]> {
-        return new None<T[O]>();
-    }
-    
-    filter(fn: FilterFunction<T>): Maybe<T> {
-        return this;
-    }
+	else(fn: () => T): Maybe<T> {
+		return Maybe(fn());
+	}
 
-    static of<TIn>(): Maybe<TIn> {
-        return new None<TIn>();
-    }
+	prop<O extends keyof T>(propName: O): Maybe<T[O]> {
+		return new None<T[O]>();
+	}
+
+	filter(fn: FilterFunction<T>): Maybe<T> {
+		return this;
+	}
+
+	static of<TIn>(): Maybe<TIn> {
+		return new None<TIn>();
+	}
 }
 
 export class Just<T> implements Maybe<T> {
-    $value: T;
-    constructor(v: T) {
-        this.$value = v;
-    }
+	$value: T;
+	constructor(v: T) {
+		this.$value = v;
+	}
 
-    value() {
-        return this.$value;
-    }
+	value() {
+		return this.$value;
+	}
 
-    unwrap() {
-        return this.$value;
-    }
+	unwrap() {
+		return this.$value;
+	}
 
-    hasValue() {
-        return true;
-    }
+	hasValue() {
+		return true;
+	}
 
-    map<R>(mapFn: MapFunction<T, Optional<R>>): Maybe<R> {
-        let newValue = mapFn(this.$value);
-        if (newValue === null || newValue === undefined) {
-            return new None<R>();
-        }
-        return new Just<R>(newValue);
-    }
+	map<R>(mapFn: MapFunction<T, Optional<R>>): Maybe<R> {
+		let newValue = mapFn(this.$value);
+		if (newValue === null || newValue === undefined) {
+			return new None<R>();
+		}
+		return new Just<R>(newValue);
+	}
 
-    match(m: Matching<T>): Maybe<T> {
-        if (m.Just) {
-            m.Just(this.$value);
-        }
+	match(m: Matching<T>): Maybe<T> {
+		if (m.Just) {
+			m.Just(this.$value);
+		}
 
-        return this;
-    }
-    
-    else(fn: () => T): Maybe<T> {
-        return this;
-    }
+		return this;
+	}
 
-    prop<O extends keyof T>(propName: O): Maybe<T[O]> {
-        return new Just(this.$value[propName]);
-    }
-    
-    filter(fn: FilterFunction<T>): Maybe<T> {
-        return fn(this.$value) ? this : new None<T>();
-    }
+	else(fn: () => T): Maybe<T> {
+		return this;
+	}
 
-    static of<TIn>(obj: TIn): Maybe<TIn> {
-        return new Just(obj);
-    }
+	prop<O extends keyof T>(propName: O): Maybe<T[O]> {
+		return new Just(this.$value[propName]);
+	}
+
+	filter(fn: FilterFunction<T>): Maybe<T> {
+		return fn(this.$value) ? this : new None<T>();
+	}
+
+	static of<TIn>(obj: TIn): Maybe<TIn> {
+		return new Just(obj);
+	}
 }
 
 export function Maybe<T>(value: Optional<T>): Maybe<T> {
-    if (value === null || value === undefined) {
-        return new None<T>();
-    }
-    return new Just(value);
+	if (value === null || value === undefined) {
+		return new None<T>();
+	}
+	return new Just(value);
 }
 
 export function prop<T, O extends keyof T>(obj: T, propName: O): Maybe<T[O]> {
-    let v = obj[propName];
-    if (v !== undefined) {
-        return new Just(v);
-    }
-    return new None<T[O]>();
+	let v = obj[propName];
+	if (v !== undefined) {
+		return new Just(v);
+	}
+	return new None<T[O]>();
 }
 
 export function compose<X, Y, Z>(fn1: (x: X) => Y, fn2: (y: Y) => Z): ((x: X) => Z) {
-    return (x: X) => fn2(fn1(x));
+	return (x: X) => fn2(fn1(x));
 }
 
 /**
@@ -188,31 +188,31 @@ export function compose<X, Y, Z>(fn1: (x: X) => Y, fn2: (y: Y) => Z): ((x: X) =>
  * @param m maybe
  */
 export function collapse<T>(m: Maybe<T>): Optional<T> {
-    if (m.hasValue()) {
-        return m.unwrap();
-    }
-    return null;
+	if (m.hasValue()) {
+		return m.unwrap();
+	}
+	return null;
 }
 
 
 export class Either<T> {
-    private $value: T | undefined;
-    private $messasge: string | undefined;
+	private $value: T | undefined;
+	private $messasge: string | undefined;
 
-    constructor(value?: T, errorMessage?: string) {
-        this.$value = value;
-        this.$messasge = errorMessage;
-    }
+	constructor(value?: T, errorMessage?: string) {
+		this.$value = value;
+		this.$messasge = errorMessage;
+	}
 
-    public HasValue(): boolean {
-        return this.$value !== undefined;
-    }
+	public HasValue(): boolean {
+		return this.$value !== undefined;
+	}
 
-    public static of<T>(value: T) {
-        return new Either<T>(value);
-    }
+	public static of<T>(value: T) {
+		return new Either<T>(value);
+	}
 
-    public static error(msg: string) {
-        return new Either<any>(undefined, msg);
-    }
+	public static error(msg: string) {
+		return new Either<any>(undefined, msg);
+	}
 }
