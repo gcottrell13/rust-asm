899ca9f4294f3fd092e272832c300d508d2d0135
Gage <inquisitorpinkie@gmail.com>
2019-03-24 13:15:31 -0700

    set up global state

---
+++ js/src/components/utils/globalState.tsx
@@ -0,0 +1,109 @@
+import React, {
+	createContext,
+	useState,
+	useContext,
+	useMemo,
+	useCallback, ReactNode,
+} from 'react';
+import { SMap } from '../../utils/utilTypes';
+
+let globals: SMap<GlobalState>;
+
+//#region Types and Interfaces
+
+interface Action<T> {
+	type: string;
+}
+
+interface GlobalState {
+	context: React.Context<any>;
+	value: any;
+}
+
+type PayloadValue<T> = [T, React.Dispatch<React.SetStateAction<T>>];
+interface Payload<T> {
+	value: PayloadValue<T>;
+}
+
+
+interface ProviderProps<T> {
+	value: Payload<T>;
+}
+
+interface ContextProviderProps<T> {
+	initialValue: T;
+	children: ReactNode;
+	provider: React.ComponentType<ProviderProps<T>>;
+}
+
+interface GlobalStateProviderProps<T> {
+	children: any;
+	initialValue: T;
+}
+
+//#endregion
+
+//#region Helpers
+
+function ContextProvider<T>({ initialValue , children, provider: Provider }: ContextProviderProps<T>) {
+	const [value, setValue] = useState(initialValue);
+	const payload = useMemo(() => ({ value: [value, setValue] as PayloadValue<T> }), [value]);
+	return <Provider value={payload}>{children}</Provider>;
+}
+
+function addGlobalsKey(current: SMap<GlobalState>, [key, value]: [string, any]) {
+	return {
+		...current,
+		[key]: { context: createContext(value), value },
+	};
+}
+
+//#endregion
+
+export function GlobalStateProvider<T extends SMap<any>>({ initialValue, children }: GlobalStateProviderProps<T>) {
+	if (!globals) {
+		globals = Object.entries(initialValue).reduce(addGlobalsKey, {});
+	}
+
+	return Object.entries(globals).reduce(
+		(
+			newChildren,
+			[
+				key,
+				{
+					context: { Provider },
+					value,
+				},
+			]
+		) => (
+			<ContextProvider key={key} provider={Provider} initialValue={value}>
+				{newChildren}
+			</ContextProvider>
+		),
+		children
+	);
+}
+
+export function useGlobalState<T>(key: string): PayloadValue<T> {
+	const payload = useContext<Payload<T>>(globals[key].context);
+
+	if (!payload) {
+		throw new Error('useGlobalState must be a descendant of <GlobalStateProvider/>');
+	}
+
+	return payload.value;
+}
+
+export function useGlobalReducer<T>(key: string, reducer: (state: T, action: Action<T>) => T): [T, any] {
+	const [state, setState] = useGlobalState<T>(key);
+
+	const dispatch = useCallback(
+		(action) => {
+			setState(currentState => reducer(currentState, action));
+			return action;
+		},
+		[reducer]
+	);
+
+	return [state, dispatch];
+}
---
+++ js/src/state/globalState.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { GlobalStateProvider, useGlobalState } from '../components/utils/globalState';
+
+/**
+ * The interface that defines global state for the program
+ */
+export interface DslWasmGlobalState {
+	dslCompiled: string;
+}
+
+/**
+ * The official wrapper of useGlobalState
+ * @param key
+ */
+export function useGlobalDslWasmState<TKey extends keyof DslWasmGlobalState, TValue extends DslWasmGlobalState[TKey]>(key: TKey)  {
+	return useGlobalState<DslWasmGlobalState[TKey]>(key);
+}
+
+
+
+//#region Wrappers for global state provider
+
+interface DslWasmStateProviderProps extends DslWasmGlobalState {
+	children: any;
+}
+
+export function DslWasmStateProvider({ children, ...state }: DslWasmStateProviderProps) {
+	return <GlobalStateProvider<DslWasmGlobalState> initialValue={state}>{children}</GlobalStateProvider>;
+}
+//#endregion
